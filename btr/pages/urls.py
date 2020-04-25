12f6f2d9e0001db2d044641d49553b0b6d21dbbc
beni1028 <beni1028@gmail.com>
2020-04-25 07:12:12 -0700

    stages

--- btr/pages/urls.py
+++ btr/pages/urls.py
@@ -3,7 +3,9 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('',views.index,name='index')
-    path('about',views.about,name='about')
+    path('',views.index,name='index'),
+    path('about',views.about,name='about'),
+    path('help',views.help,name='help')
+
 ]
 ]
